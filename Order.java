4a1fa83f060af207b4cfeb23e3341f39a1eb6100
annawidman <anna.widman98@gmail.com>
2026-04-07 15:15:39 +0200

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,3 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Order {
     
+    
+    private List<Item> items;
+
+    public Order (Item...items)
+    {
+        this.items = new ArrayList<>(List.of(items));
+        
+    }
+
 }
