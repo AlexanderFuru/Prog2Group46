a3fc64e0d355e4b3e23726f8db34908c45003656
annawidman <anna.widman98@gmail.com>
2026-04-07 17:30:16 +0200

    Update Order.java

--- Order.java
+++ Order.java
@@ -3,13 +3,16 @@ import java.util.ArrayList;
 
 public class Order {
     
-    
+    private final long orderNumber;
+    private static long counter;
     private List<Item> items;
 
     public Order (Item...items)
     {
         this.items = new ArrayList<>(List.of(items));
-        
+
+        counter++;
+        this.orderNumber = counter;
     }
 
 }
